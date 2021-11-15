ef5ccb1ec201f43cb2e7fa69a2a9f4ba7dd89ba8
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-11-15 11:46:49 +0000

    login and register design done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Footer from './App/Layouts/Footer/Footer';
+import Navbar from './App/Layouts/Navbar/Navbar';
+import HomePage from './App/Pages/HomePage/HomePage';
+import LoginScreen from './App/Pages/LoginScreen/LoginScreen';
+import ProductDetailedPage from './App/Pages/ProductDetailedPage/ProductDetailedPage';
+import RegisterPage from './App/Pages/ RegisterPage/RegisterPage';
+import ProfilePage from './App/Pages/ProfilePage/ProfilePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <ResponsiveHeader/> */}
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/search/:keyword" component={HomePage} />
+        <Route path="/page/:pageNumber" component={HomePage} />
+        <Route path="/search/:keyword/page/:pageNumber" component={HomePage} />
+
+        <Route path="/product/:id" component={ProductDetailedPage} />
+        <Route path="/cart/:id?" component={CartPage} />
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/profile" component={ProfilePage} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
