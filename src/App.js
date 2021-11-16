64d31aa4498d9d964908ff8f2b5cd500b7620eb7
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-11-16 03:11:01 +0000

    Order admin page done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import RegisterPage from './App/Pages/ RegisterPage/RegisterPage';
 import ProfilePage from './App/Pages/ProfilePage/ProfilePage';
 import PlaceOrderPage from './App/Pages/PlaceOrderPage/PlaceOrderPage';
 import OrderPage from './App/Pages/OrderPage/OrderPage';
+import UsersPage from './App/Pages/Admins/UsersPage/UsersPage';
+import EditUserPage from './App/Pages/Admins/EditUserPage/EditUserPage';
+import ProductListPage from './App/Pages/Admins/ProductsPage/ProductsPage';
+import EditProductPage from './App/Pages/Admins/EditProductPage/EditProductPage';
+import OrdersPage from './App/Pages/Admins/OrdersPage/OrdersPage';
 function App() {
   return (
     <div>
@@ -19,7 +24,6 @@ function App() {
         <Route path="/search/:keyword" component={HomePage} />
         <Route path="/page/:pageNumber" component={HomePage} />
         <Route path="/search/:keyword/page/:pageNumber" component={HomePage} />
-
         <Route path="/placeOrder" component={PlaceOrderPage} />
         <Route path="/orders/:id" component={OrderPage} />
         <Route path="/product/:id" component={ProductDetailedPage} />
@@ -27,6 +31,11 @@ function App() {
         <Route path="/login" component={LoginScreen} />
         <Route path="/register" component={RegisterPage} />
         <Route path="/profile" component={ProfilePage} />
+        <Route path="/admin/userList" component={UsersPage} />
+        <Route path="/admin/users/:id/edit" component={EditUserPage} />
+        <Route path="/admin/productsList" component={ProductListPage} />
+        <Route path="/admin/product/:id/edit" component={EditProductPage} />
+        <Route path="/admin/ordersList" component={OrdersPage} />
       </Switch>
       <Footer />
     </div>
